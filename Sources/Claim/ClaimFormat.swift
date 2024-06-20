85b9a3e3274efceca49b16f25c5498d0d7b8b752
Andreas Ganske <a.ganske@chaosspace.de>
2024-06-20 11:44:57 +0200

    Add SD-JWT VC (with additional zkp) to claim formats

--- Sources/Claim/ClaimFormat.swift
+++ Sources/Claim/ClaimFormat.swift
@@ -17,16 +17,60 @@ public enum ClaimFormat: Equatable, Hashable {
   case msoMdoc
   case jwtType(JWTType)
   case ldpType(LDPType)
-
+  case sdJWT(SDJWTType)
+    
   public enum JWTType: Equatable, Hashable {
     case jwt
     case jwt_vc
     case jwt_vp
   }
 
+  public enum SDJWTType: Equatable, Hashable {
+    case vc
+    case vc_zkp
+  }
+
   public enum LDPType: Equatable, Hashable {
     case ldp
     case ldp_vc
     case ldp_vp
+  }
+  
+  public var formatIdentifier: String {
+    switch self {
+    case .msoMdoc: "mso_mdoc"
+    case .jwtType(let jwtType):
+      switch jwtType {
+      case .jwt: "jwt"
+      case .jwt_vc: "jwt_vc"
+      case .jwt_vp: "jwt_vp"
+      }
+    case .ldpType(let ldpType):
+      switch ldpType {
+      case .ldp: "ldp"
+      case .ldp_vc: "ldp_vc"
+      case .ldp_vp: "ldp_vp"
+      }
+    case .sdJWT(let sdJWTType):
+      switch sdJWTType {
+      case .vc: "vc+sd-jwt"
+      case .vc_zkp: "vc+sd-jwt+zkp"
+      }
+    }
+  }
+  
+  public init?(formatIdentifier: String) {
+    switch formatIdentifier {
+    case "mso_mdoc": self = .msoMdoc
+    case "jwt": self = .jwtType(.jwt)
+    case "jwt_vc": self = .jwtType(.jwt_vc)
+    case "jwt_vp": self = .jwtType(.jwt_vp)
+    case "ldp": self = .ldpType(.ldp)
+    case "ldp_vc": self = .ldpType(.ldp_vc)
+    case "ldp_vp": self = .ldpType(.ldp_vp)
+    case "vc+sd-jwt": self = .sdJWT(.vc)
+    case "vc+sd-jwt+zkp": self = .sdJWT(.vc_zkp)
+    default: return nil
+    }
   }
 }
